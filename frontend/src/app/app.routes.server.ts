b1a840a4ba886cf6c82f20ad5510b5619d668830
HammamiNihed <nihedddhammami123456789@gmail.com>
2026-04-12 21:26:26 +0100

    fix: server route

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -2,15 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'projects/:id/collections',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: 'projects/:projectId/collections/:name',
-    renderMode: RenderMode.Server,
-  },
-  {
     path: '**',
-    renderMode: RenderMode.Prerender,
+    renderMode: RenderMode.Server,
   },
 ];
