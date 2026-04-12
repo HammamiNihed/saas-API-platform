618418a4e4c1d08df35439ef2a7b8c8c659487bc
HammamiNihed <nihedddhammami123456789@gmail.com>
2026-04-12 21:22:00 +0100

    fix routes

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'projects/:id/collections',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'projects/:projectId/collections/:name',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
